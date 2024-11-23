e2b6c58e351f99cc18b8b42be27ca81b90208217
mohamim360 <mohamim360@gmail.com>
2024-11-23 10:22:44 +0600

    initialize clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 
 export const metadata: Metadata = {
@@ -14,12 +15,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
+    <ClerkProvider>
+      <html lang="en">
+        <body
+        >
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
 
-      >
-        {children}
-      </body>
-    </html>
   );
 }
