b0ce409d61e6fe8ac65520b6fd16f8e59d16e1f3
mohamim360 <mohamim360@gmail.com>
2024-12-15 20:11:41 +0600

    firebase,env

--- firebase.ts
+++ firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyApvI5yakKVoBnFE5U0RGH0BZzEQGudEWY",
-  authDomain: "linkedin-clone-38895.firebaseapp.com",
-  projectId: "linkedin-clone-38895",
-  storageBucket: "linkedin-clone-38895.firebasestorage.app",
-  messagingSenderId: "845240082725",
-  appId: "1:845240082725:web:099bf9429b73017ae5facc",
-  measurementId: "G-HWCV90K18N",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
